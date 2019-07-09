267c4931135cee1ce3d8066fe995c99d29f9f0f2
Casey Kolderup <casey@kolderup.org>
2019-07-08 23:09:36 -0700

    fifty shades bot works

---
+++ bots/fifty-shades/README.md
@@ -0,0 +1,4 @@
+# FIFTY SHADES OF GREY
+
+* https://twitter.com/desires_exe
+* https://botsin.space/@desires_exe
---
+++ bots/fifty-shades/app.rb
@@ -0,0 +1,86 @@
+require 'twitter'
+require 'mastodon'
+require 'tempfile'
+require 'rmagick'
+require 'dotenv'
+require 'ostruct'
+include Magick
+
+Dotenv.load
+
+def random_text
+  ["My desires are... Unconventional",
+    "So show me",
+    "Oh my god",
+    "No way"].sample
+end
+
+def search_url(query)
+  "https://api.imgur.com/3/gallery/search?q=#{query}"
+end
+
+def curl_cmd(client_id, url)
+  "curl -s -H \"Authorization: Client-ID #{client_id}\" \"#{url}\""
+end
+
+def random_noun
+  File.readlines('./words.txt').sample.chomp
+end
+
+def random_imgur_url
+  noun = random_noun
+  json = `#{curl_cmd(ENV['IMGUR_CLIENT_ID'], search_url(noun))}`
+  response = JSON.parse(json, symbolize_names: true,
+                              object_class: OpenStruct)
+  sfw_urls = response.data.reject(&:nsfw)
+                          .reject(&:animated)
+                          .select(&:height)
+                          .select { |i| i.height.fdiv(i.width) > 1.2 }
+                          .map(&:link)
+
+  choice = sfw_urls.sample
+  puts "#{noun} resulted in #{choice}"
+  choice
+end
+
+def image(url)
+  file = Tempfile.new('last_panel')
+  file.write(`curl -s #{url}`)
+  file.rewind
+  bin = File.open(file,'r'){ |f| f.read }
+  image = Image.from_blob(bin).first
+  image.change_geometry!('500x') { |c,r,i| i.resize!(c,r) }
+
+
+  template = Image.read("./template.png").first
+  combined = (ImageList.new << template << image).append(true)
+
+  file.write(combined.to_blob)
+  file.rewind
+  file
+end
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_OAUTH_TOKEN']
+  config.access_token_secret = ENV['TWITTER_OAUTH_SECRET']
+end
+
+mastodon_client = Mastodon::REST::Client.new(base_url: 'https://botsin.space', bearer_token: ENV['MASTODON_ACCESS_KEY'])
+
+text = random_text
+the_image = image(random_imgur_url)
+
+# post to Twitter
+begin
+  tries ||= 5
+  client.update_with_media(text, the_image)
+rescue Twitter::Error => e
+  retry unless (tries -= 1).zero?
+end
+
+# post to Mastodon
+media = mastodon_client.upload_media(the_image)
+mastodon_client.create_status(text, media_ids: [media.id])
+
